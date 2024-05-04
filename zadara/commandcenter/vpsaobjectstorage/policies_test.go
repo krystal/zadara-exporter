f6afd75dcd0cf4e1ae0395d95ad940f6ed548b02
Bill Gale <bill@gale.dev>
2024-05-04 17:13:11 +0100

    wip: add response tests

--- zadara/commandcenter/vpsaobjectstorage/policies_test.go
+++ zadara/commandcenter/vpsaobjectstorage/policies_test.go
@@ -52,3 +52,103 @@ func TestClient_GetStoragePolicies(t *testing.T) {
 	assert.Len(t, resp.ZiosStoragePolicies, 2)
 	assert.Equal(t, 2, resp.Count)
 }
+
+//nolint:funlen // most of this length is due to the test data
+func TestZiosStoragePoliciesResponse(t *testing.T) {
+	t.Parallel()
+
+	testJSON := `{
+		"status": "success",
+		"zios_storage_policies": [
+		  {
+			"id": "26",
+			"internal_name": "storage-policy-00000002",
+			"name": "3-way-protection-policy",
+			"status": "initialised",
+			"protection": "3-way",
+			"default": null,
+			"health_status": "normal",
+			"health_percentage": 100,
+			"ring_balance": {
+			  "normal_percentage": 99,
+			  "degraded_percentage": 0,
+			  "critical_percentage": 0,
+			  "normal_count": 32729,
+			  "degraded_count": 39,
+			  "critical_count": 0
+			},
+			"used_capacity": 0,
+			"free_capacity": 299573968896
+		  },
+		  {
+			"name": "MetadataPolicy",
+			"status": "initialised",
+			"protection": "3-way",
+			"default": null,
+			"health_status": "normal",
+			"health_percentage": 100,
+			"ring_balance": {
+			  "normal_percentage": 84,
+			  "degraded_percentage": 15,
+			  "critical_percentage": 0,
+			  "normal_count": 3465,
+			  "degraded_count": 631,
+			  "critical_count": 0
+			},
+			"used_capacity": 38377881,
+			"free_capacity": 236184823399
+		  }
+		],
+		"count": 2
+	  }`
+
+	var resp vpsaobjectstorage.ZiosStoragePoliciesResponse
+
+	require.NoError(t, json.Unmarshal([]byte(testJSON), &resp))
+
+	assert.Equal(t, "success", resp.Status)
+
+	assert.Len(t, resp.ZiosStoragePolicies, 2)
+
+	assert.Equal(t, &vpsaobjectstorage.ZiosStoragePolicy{
+		ID:               "26",
+		InternalName:     "storage-policy-00000002",
+		Name:             "3-way-protection-policy",
+		Status:           "initialised",
+		Protection:       "3-way",
+		Default:          nil,
+		HealthStatus:     "normal",
+		HealthPercentage: 100,
+		RingBalance: vpsaobjectstorage.RingBalance{
+			NormalPercentage:   99,
+			DegradedPercentage: 0,
+			CriticalPercentage: 0,
+			NormalCount:        32729,
+			DegradedCount:      39,
+			CriticalCount:      0,
+		},
+		UsedCapacity: 0,
+		FreeCapacity: 299573968896,
+	}, resp.ZiosStoragePolicies[0])
+
+	assert.Equal(t, &vpsaobjectstorage.ZiosStoragePolicy{
+		Name:             "MetadataPolicy",
+		Status:           "initialised",
+		Protection:       "3-way",
+		Default:          nil,
+		HealthStatus:     "normal",
+		HealthPercentage: 100,
+		RingBalance: vpsaobjectstorage.RingBalance{
+			NormalPercentage:   84,
+			DegradedPercentage: 15,
+			CriticalPercentage: 0,
+			NormalCount:        3465,
+			DegradedCount:      631,
+			CriticalCount:      0,
+		},
+		UsedCapacity: 38377881,
+		FreeCapacity: 236184823399,
+	}, resp.ZiosStoragePolicies[1])
+
+	assert.Equal(t, 2, resp.Count)
+}
--- zadara/commandcenter/vpsaobjectstorage/stores_test.go
+++ zadara/commandcenter/vpsaobjectstorage/stores_test.go
@@ -51,3 +51,115 @@ func TestClient_GetStores(t *testing.T) {
 	assert.Len(t, resp.Zioses, 2)
 	assert.Equal(t, 2, resp.Count)
 }
+
+func TestZiosResponse(t *testing.T) {
+	t.Parallel()
+
+	testJSON := `{
+		"status": "success",
+		"zioses": [
+		  {
+			"id": 188,
+			"name": "zios",
+			"internal_name": "vsa-0000016d",
+			"user": "john",
+			"tenant_name": "user_john_Gt1cj",
+			"description": "zios",
+			"status": "Normal",
+			"engine_type": "ZIOS",
+			"vcpus": 3,
+			"ram": 6144,
+			"https_termination": true,
+			"image": "zios-00.00-434.img",
+			"drives": 5,
+			"cache": 30,
+			"virtual_controllers": 3,
+			"ip_address": "150.50.2.130",
+			"public_ip": null,
+			"management_url": "vsa-0000016d-zadara-dev2.zadarazios.com",
+			"storage_policies_count": 1,
+			"metadata_policies_count": 1,
+			"accounts_count": 1,
+			"users_count": 3,
+			"containers_count": 0,
+			"objects_count": 0,
+			"network_configuration": {
+			  "vc0": {
+				"fe_ip": "150.50.2.113",
+				"be_ip": "150.50.3.113",
+				"hb_ip": "150.50.0.113"
+			  },
+			  "vc1": {
+				"fe_ip": "150.50.2.114",
+				"be_ip": "150.50.3.114",
+				"hb_ip": "150.50.0.114"
+			  },
+			  "vc2": {
+				"fe_ip": "150.50.2.118",
+				"be_ip": "150.50.3.118",
+				"hb_ip": "150.50.0.118"
+			  }
+			},
+			"created_at": "2016-04-15 20:22:10 UTC",
+			"updated_at": "2016-04-18 13:42:48 UTC"
+		  }
+		],
+		"count": 1
+	  }`
+
+	var resp vpsaobjectstorage.ZiosResponse
+
+	require.NoError(t, json.Unmarshal([]byte(testJSON), &resp))
+
+	assert.Equal(t, "success", resp.Status)
+
+	assert.Len(t, resp.Zioses, 1)
+
+	assert.Equal(t, &vpsaobjectstorage.Zios{
+		ID:                    188,
+		Name:                  "zios",
+		InternalName:          "vsa-0000016d",
+		User:                  "john",
+		TenantName:            "user_john_Gt1cj",
+		Description:           "zios",
+		Status:                "Normal",
+		EngineType:            "ZIOS",
+		Vcpus:                 3,
+		RAM:                   6144,
+		HTTPSTermination:      true,
+		Image:                 "zios-00.00-434.img",
+		Drives:                5,
+		Cache:                 30,
+		VirtualControllers:    3,
+		IPAddress:             "150.50.2.130",
+		PublicIP:              nil,
+		ManagementURL:         "vsa-0000016d-zadara-dev2.zadarazios.com",
+		StoragePoliciesCount:  1,
+		MetadataPoliciesCount: 1,
+		AccountsCount:         1,
+		UsersCount:            3,
+		ContainersCount:       0,
+		ObjectsCount:          0,
+		NetworkConfiguration: map[string]vpsaobjectstorage.NetworkConfiguration{
+			"vc0": {
+				FeIP: "150.50.2.113",
+				BeIP: "150.50.3.113",
+				HbIP: "150.50.0.113",
+			},
+			"vc1": {
+				FeIP: "150.50.2.114",
+				BeIP: "150.50.3.114",
+				HbIP: "150.50.0.114",
+			},
+			"vc2": {
+				FeIP: "150.50.2.118",
+				BeIP: "150.50.3.118",
+				HbIP: "150.50.0.118",
+			},
+		},
+		CreatedAt: "2016-04-15 20:22:10 UTC",
+		UpdatedAt: "2016-04-18 13:42:48 UTC",
+	}, resp.Zioses[0])
+
+	assert.Equal(t, 1, resp.Count)
+}
