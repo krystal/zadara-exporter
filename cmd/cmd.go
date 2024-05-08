a8580d8ccfed97a18d5162c16a93b12325dabf69
Bill Gale <bill@gale.dev>
2024-05-08 12:22:42 +0100

    wip: add version and commit into binary

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,19 +2,23 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-// var (
-// 	Version = "0.0.0-dev"
-// ).
+var (
+	version = "0.0.0-dev"
+	commit  = ""
+)
 
 // NewRootCommand creates a new root command for the zadara-exporter.
 func NewRootCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "zadara-exporter",
-		Short: "Zadara exporter for Prometheus",
+		Use:     "zadara-exporter",
+		Short:   "Zadara exporter for Prometheus",
+		Version: fmt.Sprintf("%s (%s)", version, commit),
 	}
 
 	cmd.PersistentFlags().String("config", "", "The path to the configuration file")
